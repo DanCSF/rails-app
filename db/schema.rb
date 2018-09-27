289c7471af96ce0625c1db4a093c6443a692774f
DanCSF <m210amg@gmail.com>
2018-09-27 21:09:55 +0300

    cost scaffold and styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_145530) do
+ActiveRecord::Schema.define(version: 2018_09_27_180501) do
+
+  create_table "costs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "age"
+    t.string "babysitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hosts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_145530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "age"
+    t.string "address"
     t.string "street"
     t.string "babysitter_name"
   end
